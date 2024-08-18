efef4d55b822c12873b0ab9ac8a82560a176fee8
Chris Reimann <c-reimann@hotmail.de>
2024-08-18 14:16:31 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='curiosity_gym', version='0.0.1', packages=find_packages(include=['core', 'envs']))
+setup(name='curiosity_gym', version='0.0.1', packages=["curiosity_gym"])
